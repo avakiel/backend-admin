efb941248037ead438f202dedd73cba8e88b09c5
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2024-03-21 17:26:37 +0200

    added autocomplete

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,40 +1,69 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import { Tabs, Tab, Typography, Box } from '@mui/material';
-import ProductsTable from './ProductTable';
-import { Product, SelectedPhone } from '@/types/Product';
+import React, { useState } from "react";
+import { Tabs, Tab, Typography, Box, Autocomplete, TextField } from "@mui/material";
+import ProductsTable from "./ProductTable";
+import { Product, SelectedPhone } from "@/types/Product";
 
 interface Props {
   products: SelectedPhone[];
 }
 
+interface AutocompleteOption {
+  label: string;
+  id: string;
+}
+
 const Main: React.FC<Props> = ({ products }) => {
   const [value, setValue] = useState<number>(0);
+  const [selectedProduct, setSelectedProduct] = useState<AutocompleteOption | null>(null);
+
+  const options = products.map((item) => ({
+    label: item.name,
+    id: item.id
+  }));
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
-  const tabContents = [<ProductsTable products={products} key={'itemOne'} />, "Item Two", "Item Three"];
+  const handleProductChange = (event: React.ChangeEvent<{}>, newValue: AutocompleteOption | null) => {
+    if (newValue) {
+      setSelectedProduct(newValue);
+    } else {
+      setSelectedProduct(null);
+    }
+  };
+
+  const preparedProducts = selectedProduct ? products.filter((item) => item.id === selectedProduct?.id) : products;
+
+  const tabContents = [<ProductsTable products={preparedProducts} key={"itemOne"} />, "Item Two", "Item Three"];
 
   return (
-    <Box sx={{ flexGrow: 1, display: 'flex', width: '100%', backgroundColor: 'DimGray' }}>
+    <Box sx={{ flexGrow: 1, display: "flex", width: "100%", backgroundColor: "DimGray" }}>
       <Tabs
         orientation="vertical"
         variant="scrollable"
         value={value}
         onChange={handleChange}
         aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider', '& .MuiTabs-indicator': { backgroundColor: 'white', width: 5 } }}
+        sx={{ borderRight: 1, borderColor: "divider", "& .MuiTabs-indicator": { backgroundColor: "white", width: 5 } }}
       >
-        <Tab label="Phones" style={{ color: value === 0 ? 'white' : 'black' }} />
-        <Tab label="Tablets" style={{ color: value === 1 ? 'white' : 'black' }} />
-        <Tab label="Accessories" style={{ color: value === 2 ? 'white' : 'black' }} />
+        <Tab label="Phones" style={{ color: value === 0 ? "white" : "black" }} />
+        <Tab label="Tablets" style={{ color: value === 1 ? "white" : "black" }} />
+        <Tab label="Accessories" style={{ color: value === 2 ? "white" : "black" }} />
       </Tabs>
+
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={options}
+        onChange={handleProductChange}
+        sx={{ width: 200 }}
+        renderInput={(params) => <TextField {...params} label="Find 1 Product" />}
+      />
       {tabContents[value]}
     </Box>
-
   );
 };
 
