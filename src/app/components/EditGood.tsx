c1d582da7601ab5e7caf554af9df93cc64937b5c
Maksym Kalashnyk <maxplay123890@gmail.com>
2024-03-24 23:02:02 +0200

    add unfinished edit

---
+++ src/app/components/EditGood.tsx
@@ -0,0 +1,194 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  Typography,
+  Modal,
+  IconButton,
+  Button,
+  FormControl,
+  InputLabel,
+  Input,
+  MenuItem,
+  Select,
+} from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import { Product } from '@prisma/client';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
+
+interface Props {
+  product: Product;
+  onSave: (updatedProduct: Product) => void;
+}
+
+const EditGood: React.FC<Props> = ({ product, onSave }) => {
+  const [open, setOpen] = useState(false);
+  const [editedProduct, setEditedProduct] = useState<Product>(product);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSave(editedProduct);
+    handleClose();
+  };
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setEditedProduct((prevProduct) => ({
+      ...prevProduct,
+      [name]: value,
+    }));
+  };
+  
+  const handleSelectChange = (
+    event: React.ChangeEvent<{ name?: string | undefined; value: unknown }>
+  ) => {
+    const { name, value } = event.target;
+    setEditedProduct((prevProduct) => ({
+      ...prevProduct,
+      [name as string]: value as string,
+    }));
+  };  
+
+  return (
+    <div>
+      <IconButton size="large" aria-label="edit" onClick={handleOpen}>
+        <EditIcon />
+      </IconButton>
+
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
+          <Typography variant="h6" align="center" gutterBottom>
+            Edit Product
+          </Typography>
+
+          <form onSubmit={handleFormSubmit}>
+            <FormControl fullWidth sx={{ mb: 4 }}>
+              <InputLabel htmlFor="name">Name</InputLabel>
+              <Input
+                id="name"
+                name="name"
+                value={editedProduct.name}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+
+            <FormControl fullWidth sx={{ mb: 4 }}>
+              <InputLabel htmlFor="capacity"  sx={{ marginTop: '-0.5rem' }}>Capacity</InputLabel>
+              <Select
+                id="capacity"
+                name="capacity"
+                value={editedProduct.capacity}
+                onChange={handleSelectChange}
+              >
+                <MenuItem value="32GB">32GB</MenuItem>
+                <MenuItem value="64GB">64GB</MenuItem>
+                <MenuItem value="128GB">128GB</MenuItem>
+                <MenuItem value="256GB">256GB</MenuItem>
+                <MenuItem value="512GB">512GB</MenuItem>
+              </Select>
+            </FormControl>
+
+            <FormControl fullWidth sx={{ mb: 4 }}>
+              <InputLabel htmlFor="priceRegular">Price Regular</InputLabel>
+              <Input
+                id="priceRegular"
+                name="priceRegular"
+                value={editedProduct.fullPrice}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+
+            <FormControl fullWidth sx={{ mb: 4 }}>
+              <InputLabel htmlFor="priceDiscount">Price with Discount</InputLabel>
+              <Input
+                id="priceDiscount"
+                name="priceDiscount"
+                value={editedProduct.price}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+
+            <FormControl fullWidth sx={{ mb: 4}}>
+            <InputLabel htmlFor="color"  sx={{ marginTop: '-0.5rem' }}>Color</InputLabel>
+            <Select
+              id="color"
+              name="color"
+              value={editedProduct.color}
+              onChange={handleSelectChange}
+            >
+              <MenuItem value="green">Green</MenuItem>
+              <MenuItem value="red">Red</MenuItem>
+              <MenuItem value="white">White</MenuItem>
+              <MenuItem value="gold">Gold</MenuItem>
+              <MenuItem value="spacegray">Space Gray</MenuItem>
+              <MenuItem value="yellow">Yellow</MenuItem>
+              <MenuItem value="mightgreen">Mightgreen</MenuItem>
+            </Select>
+            </FormControl>
+
+            <FormControl fullWidth sx={{ mb: 4 }}>
+            <InputLabel htmlFor="screen" sx={{ marginTop: '-0.5rem' }}>Screen</InputLabel>
+              <Select
+                id="screen"
+                name="screen"
+                value={editedProduct.screen}
+                onChange={handleSelectChange}
+              >
+                <MenuItem value="6.5' OLED">6.5' OLED</MenuItem>
+                <MenuItem value="5.8' OLED">5.8' OLED</MenuItem>
+                <MenuItem value="6.1' IPS">6.1' IPS</MenuItem>
+                <MenuItem value="5.5' IPS">5.5' IPS</MenuItem>
+              </Select>
+            </FormControl>
+
+
+            <FormControl fullWidth sx={{ mb: 4 }}>
+              <InputLabel htmlFor="ram" sx={{ marginTop: '-0.5rem' }}>RAM</InputLabel>
+              <Select
+                id="ram"
+                name="ram"
+                value={editedProduct.ram}
+                onChange={handleSelectChange}
+              >
+                <MenuItem value="3GB">3GB</MenuItem>
+                <MenuItem value="4GB">4GB</MenuItem>
+              </Select>
+            </FormControl>
+
+            <FormControl fullWidth sx={{ mb: 4 }}>
+              <InputLabel htmlFor="year">Year</InputLabel>
+              <Input
+                id="year"
+                name="year"
+                value={editedProduct.year}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+              <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+                Save Changes
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default EditGood;
--- src/app/components/ProductTable.tsx
+++ src/app/components/ProductTable.tsx
@@ -1,9 +1,18 @@
-"use client";
-
 import React, { useState } from "react";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton, TablePagination } from "@mui/material";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  IconButton,
+  TablePagination,
+} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Product } from "@prisma/client";
+import EditGood from './EditGood';
 
 interface Props {
   products: Product[];
@@ -21,63 +30,71 @@ const ProductsTable: React.FC<Props> = ({ products }) => {
   };
 
   return (
-      <TableContainer component={Paper} style={{ width: '100%' }}>
-        <Table sx={{ minWidth: 650, backgroundColor: 'gray', padding: 2 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-               {/* <TableCell padding="checkbox">
-              <Checkbox
-                color="primary"
-                indeterminate={numSelected > 0 && numSelected < rowCount}
-                checked={rowCount > 0 && numSelected === rowCount}
-                inputProps={{
-                  'aria-label': 'select all desserts',
-                }}
-            />
-          </TableCell> */}
-              <TableCell>Delete</TableCell>
-              <TableCell>ID</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Capacity</TableCell>
-              <TableCell>Price Regular</TableCell>
-              <TableCell>Price with Discount</TableCell>
-              <TableCell>Color</TableCell>
-              <TableCell>Screen</TableCell>
-              <TableCell>RAM</TableCell>
-              <TableCell>Year</TableCell>
+    <TableContainer component={Paper} style={{ width: '100%' }}>
+      <Table sx={{ minWidth: 650, backgroundColor: 'gray', padding: 2 }} aria-label="simple table">
+        <TableHead>
+        <TableRow>
+        {/* <TableCell padding="checkbox">
+        <Checkbox
+        color="primary"
+        indeterminate={numSelected > 0 && numSelected < rowCount}
+        checked={rowCount > 0 && numSelected === rowCount}
+        inputProps={{
+        'aria-label': 'select all desserts',
+        }}
+        />
+        </TableCell> */}
+            <TableCell>Delete</TableCell>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Capacity</TableCell>
+            <TableCell>Price Regular</TableCell>
+            <TableCell>Price with Discount</TableCell>
+            <TableCell>Color</TableCell>
+            <TableCell>Screen</TableCell>
+            <TableCell>RAM</TableCell>
+            <TableCell>Year</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {products.slice(page * 15, page * 15 + 15).map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>
+                <IconButton aria-label="delete" size="large">
+                  <DeleteIcon fontSize="inherit" />
+                </IconButton>
+                <IconButton aria-label="delete" size="large">
+                  <EditGood
+                    product={product}
+                    onSave={(updatedProduct) => {
+                      console.log('Updated product:', updatedProduct);
+                    }}
+                  />
+                </IconButton>
+              </TableCell>
+              <TableCell>{product.id}</TableCell>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.capacity}</TableCell>
+              <TableCell>{product.fullPrice}</TableCell>
+              <TableCell>{product.price}</TableCell>
+              <TableCell>{product.color}</TableCell>
+              <TableCell>{product.screen}</TableCell>
+              <TableCell>{product.ram}</TableCell>
+              <TableCell>{product.year}</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {products.slice(page * 15, page * 15 + 15).map((product) => (
-              <TableRow key={product.id}>
-                <TableCell>
-                  <IconButton aria-label="delete" size="large">
-                    <DeleteIcon fontSize="inherit" />
-                  </IconButton>
-                </TableCell>
-                <TableCell>{product.id}</TableCell>
-                <TableCell>{product.name}</TableCell>
-                <TableCell>{product.capacity}</TableCell>
-                <TableCell>{product.fullPrice}</TableCell>
-                <TableCell>{product.price}</TableCell>
-                <TableCell>{product.color}</TableCell>
-                <TableCell>{product.screen}</TableCell>
-                <TableCell>{product.ram}</TableCell>
-                <TableCell>{product.year}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        <TablePagination
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
         rowsPerPage={15}
         component="div"
         count={products.length}
         page={page}
         onPageChange={handleChangePage}
         labelRowsPerPage=""
-        SelectProps={{ 
+        SelectProps={{
           native: true,
-          IconComponent: () => null, 
+          IconComponent: () => null,
           style: { display: 'none' }
         }}
         sx={{ backgroundColor: 'gray' }}
@@ -87,4 +104,3 @@ const ProductsTable: React.FC<Props> = ({ products }) => {
 };
 
 export default ProductsTable;
-
