6d81ef805c0f11bdadaf118e53ca8358bdd55a2e
avakiel <79792776+avakiel@users.noreply.github.com>
2024-04-02 20:53:56 +0300

    new db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,43 +1,59 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const data = require('../seed_data/products/products.json');
+const products = require('../seed_data/products/products.json');
+const phoneDetails = require('../seed_data/products/phone.json');
+const tabletDetails = require('../seed_data/products/tablets.json');
+const accessoriesDetails = require('../seed_data/products/accessories.json');
 
-async function main() {
-  for (const record of data) {
-    try {
-      let category = await prisma.category.findFirst({
-        where: { name: record.category },
-      });
+async function seed() {
+  const categories = [
+    { name: 'Phone' },
+    { name: 'Tablet' },
+    { name: 'Accessories' }
+  ];
 
-      if (!category) {
-        category = await prisma.category.create({
-          data: { name: record.category },
-        });
-      }
+  for (const category of categories) {
+    await prisma.category.upsert({
+      where: { name: category.name },
+      update: {},
+      create: category,
+    });
+  }
 
-      await prisma.product.create({
-        data: {
-          itemId: record.itemId,
-          name: record.name,
-          fullPrice: record.fullPrice,
-          price: record.price,
-          screen: record.screen,
-          capacity: record.capacity,
-          color: record.color,
-          ram: record.ram,
-          year: record.year,
-          image: record.image,
-          categoryId: category.id,
-        },
-      });
-    } catch (error) {
-      console.error('Error seeding database:', error);
+  for (const product of products) {
+    let category;
+    switch (product.category) {
+      case 'Phone':
+        category = phoneDetails[product.year];
+        break;
+      case 'Tablet':
+        category = tabletDetails[product.year];
+        break;
+      case 'Accessories':
+        category = accessoriesDetails[product.year];
+        break;
+      default:
+        throw new Error('Unknown category');
     }
+
+    await prisma.product.create({
+      data: {
+        ...product,
+        categoryId: category.id,
+      },
+    });
   }
 }
 
-main()
-  .finally(async () => {
+async function main() {
+  try {
+    await seed();
+    console.log('Data seeded successfully!');
+  } catch (error) {
+    console.error('Error seeding data:', error);
+  } finally {
     await prisma.$disconnect();
-  });
+  }
+}
 
+main();
